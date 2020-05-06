fa022a585cd524315f1c4510bf718db42658dc57
jdom32 <jl_0163@hotmail.com>
2020-05-06 20:22:08 +0100

    initial commit

---
+++ boards/views.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.shortcuts import render
+
+# Create your views here.
+from django.http import HttpResponse
+from .models import Board
+
+
+def home(request):
+	boards = Board.objects.all()
+	return render(request, 'home.html', {'boards': boards})
