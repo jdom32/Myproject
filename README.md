53c65ed457a910046dc8ca116f26d800784c6c8a
jdom32 <jl_0163@hotmail.com>
2020-05-08 16:25:20 +0100

    module 3 of the sample

--- README.md
+++ README.md
@@ -2,3 +2,5 @@
 # python manage.py runserver
 
 # python manage.py migrate
+
+#DOUBT: tests.py -> 'href="{0}"'.format(homepage_url) what does "{0}" mean?
--- myproject/urls.py
+++ myproject/urls.py
@@ -19,6 +19,7 @@ from django.contrib import admin
 from boards import views
 
 urlpatterns = [
-	url(r'^homepage/$',views.home,name='home'),    
+	url(r'^homepage/$', views.home, name='home'),
+	url(r'^boards/(?P<pk>\d+)/$', views.board_topics, name='board_topics'),
 	url(r'^admin/', admin.site.urls),
 ]
